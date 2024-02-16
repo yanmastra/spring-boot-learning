3e8f3b480262dd9de503b25880889364e04e865d
Wayan Mastra <wayan.mastra@gada.io>
2024-02-16 16:08:10 +0800

    initial commmit
    Signed-off-by: Wayan Mastra <wayan.mastra@gada.io>

---
+++ microservices/src/main/java/com/yanmastra/microservices/MicroservicesApplication.java
@@ -0,0 +1,11 @@
+package com.yanmastra.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+}
